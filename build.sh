eb675a12d713125261a8245951c262a4e9fd76b9
puskyer <puskyer@gmail.com>
2014-03-25 09:15:23 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-dobuildlog=/tmp/dobuild.log
+build=atrix
+device=olympus
+
+dobuildlog=/tmp/dobuild$build$device`date +%d%m%Y`.log
 boardolympuspanel=./kernel/motorola/olympus/arch/arm/mach-tegra/board-olympus-panel.c
 boardolympuspanel_tmp=/tmp/board-olympus-panel.c
 
@@ -14,26 +17,29 @@ testreszaner=" .h_sync_width = 16,"
 #
 #
 
-echo "Start do repo sync & build atrix  `date`">>$dobuildlog
+echo "Start do repo sync & build $build $device  `date`">>$dobuildlog
 .repo/repo/repo sync -j2  >>$dobuildlog 2>&1
 
+if [ "$build" == "atrix" ] ; then
 
-if [ "$1" = "zaner" ] ; then
-   echo "doing Zaner's"
-   if [ "$testrespusky" == "$testfile" ] ; then
-     sed -e 's/\.h_sync_width = 4/\.h_sync_width = 16/'  -e 's/\.h_back_porch = 52/\.h_back_porch = 32/'  -e 's/\.h_front_porch = 52/\.h_front_porch = 32/' $boardolympuspanel  >$boardolympuspanel_tmp
-     cp $boardolympuspanel_tmp  $boardolympuspanel
+   if [ "$1" = "zaner" ] ; then
+      echo "doing Zaner's"
+      if [ "$testrespusky" == "$testfile" ] ; then
+         sed -e 's/\.h_sync_width = 4/\.h_sync_width = 16/'  -e 's/\.h_back_porch = 52/\.h_back_porch = 32/'  -e 's/\.h_front_porch = 52/\.h_front_porch = 32/' $boardolympuspanel  >$boardolympuspanel_tmp
+         cp $boardolympuspanel_tmp  $boardolympuspanel
+      fi
+   else
+      echo "doing Pusky's"
+      if [ "$testreszaner" == "$testfile" -o "" ==  "$testfile" ] ; then
+         sed -e 's/\.h_sync_width = 16/\.h_sync_width = 4/'  -e 's/\.h_back_porch = 32/\.h_back_porch = 52/'  -e 's/\.h_front_porch = 32/\.h_front_porch = 52/' $boardolympuspanel >$boardolympuspanel_tmp
+         cp $boardolympuspanel_tmp  $boardolympuspanel
+      fi
    fi
-else
-   echo "doing Pusky's"
-   if [ "$testreszaner" == "$testfile" -o "" ==  "$testfile" ] ; then
-     sed -e 's/\.h_sync_width = 16/\.h_sync_width = 4/'  -e 's/\.h_back_porch = 32/\.h_back_porch = 52/'  -e 's/\.h_front_porch = 32/\.h_front_porch = 52/' $boardolympuspanel >$boardolympuspanel_tmp
-     cp $boardolympuspanel_tmp  $boardolympuspanel
-   fi
-fi
+if
 
 source build/envsetup.sh    >>$dobuildlog 2>&1
-breakfast olympus  >>$dobuildlog 2>&1     # for atrix
+breakfast $device  >>$dobuildlog 2>&1     # for atrix
 time make -j3 bacon   >>$dobuildlog 2>&1  # j3 is number of cores
 ##. build/envsetup.sh && brunch model_name
-echo "End do repo sync & build atrix  `date`">>$dobuildlog
+
+echo "End do repo sync & build $build $device  `date`">>$dobuildlog
